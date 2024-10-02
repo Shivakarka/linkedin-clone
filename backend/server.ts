1f87ec4a55be3b22b6002b4cf8b47f2e51b4e101
shivakarka <shivakarka@gmail.com>
2024-10-02 23:39:25 +0530

    initial commit

---
+++ backend/server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const MongoDBUrl = process.env.MONGODB_URI ||'mongodb://localhost/127.0.0.1:27017/test';
+
+mongoose.connect(MongoDBUrl)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+
+export default app;
