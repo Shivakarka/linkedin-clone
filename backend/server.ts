1e5589b29ad1492ad79afbdeb7e8835233f8766e
shivakarka <shivakarka@gmail.com>
2024-10-26 18:56:17 +0530

    Add cors origin rule

--- backend/server.ts
+++ backend/server.ts
@@ -15,7 +15,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
