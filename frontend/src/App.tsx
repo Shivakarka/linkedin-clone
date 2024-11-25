3b56052e49461c4a3f64a83e4c8b58c4967fd804
shivakarka <shivakarka@gmail.com>
2024-11-25 23:04:13 +0530

    Add Signup and Login pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,41 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/auth/SignUpPage";
 import LoginPage from "./pages/auth/LoginPage";
+import {toast, Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
 
 function App() {
+
+  const { data: authUser, isPending } = useQuery({
+		queryKey: ["authUser"],
+		queryFn: async () => {
+			try {
+				const res = await axiosInstance.get("/auth/me");
+        console.log(res.data);
+				return res.data;
+			} catch (err : any) {
+				if (err.response && err.response.status === 401) {
+					return null;
+				}
+				toast.error(err.response.data.message || "Something went wrong");
+			}
+		},
+	});
+
+	if (isPending) return null;
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
       </Routes>
+      <Toaster />
     </Layout>
   );
 }
---
+++ frontend/src/components/auth/SignUpForm.tsx
@@ -0,0 +1,87 @@
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { axiosInstance } from "../../lib/axios";
+import {toast} from "react-hot-toast";
+import { Loader } from "lucide-react";
+
+type FormParams = {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+}
+
+const SignUpForm = () => {
+
+const [name,setName] = useState('');
+const [email,setEmail] = useState('');
+const [username,setUsername] = useState('');
+const [password,setPassword] = useState('');
+
+const {mutate: signUpMutation, isPending} = useMutation({
+    mutationFn: async(data:FormParams) => {
+        const response = await axiosInstance.post('/auth/signup',data);
+        return response.data;
+    },
+    onSuccess: (data) => {
+        toast.success(data?.message);
+    },
+    onError: (error) => {            
+        toast.error((error as any)?.response?.data?.message);
+    }
+})
+const handleSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    signUpMutation({
+        name,
+        username,
+        email,
+        password
+    })
+}
+
+  return (
+    <form onSubmit={handleSignUp} className='flex flex-col gap-4'>
+        <input
+				type='text'
+				placeholder='Full name'
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+				className='input input-bordered w-full'
+				required
+			/>
+			<input
+				type='text'
+				placeholder='Username'
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+				className='input input-bordered w-full'
+				required
+			/>
+			<input
+				type='email'
+				placeholder='Email'
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+				className='input input-bordered w-full'
+				required
+			/>
+			<input
+				type='password'
+				placeholder='Password (6+ characters)'
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+				className='input input-bordered w-full'
+				required
+			/>
+
+            <button type='submit' disabled={isPending} className='btn btn-primary w-full text-white'>
+				{isPending ? <Loader className='size-5 animate-spin' /> : "Agree & Join"}
+			</button>
+
+    </form>
+  )
+}
+
+export default SignUpForm;
