3aecb7ac34ee7ea38c1663fe60c408007f311cf9
shivakarka <shivakarka@gmail.com>
2025-01-26 00:44:26 +0530

    Update axios base URL to point to the production backend

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://linkedin-clone-backend.vercel.app",
   withCredentials: true,
 });
