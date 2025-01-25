c095edd432302b84422aad0fa944ad522828a0f5
shivakarka <shivakarka@gmail.com>
2025-01-26 01:36:37 +0530

    Refactor CORS config

--- backend/server.ts
+++ backend/server.ts
@@ -23,20 +23,8 @@ app.use(
       process?.env?.CLIENT_URL ?? "",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
-    exposedHeaders: ["set-cookie"],
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
   })
 );
-
-// Add specific CORS headers for all routes
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  next();
-});
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
